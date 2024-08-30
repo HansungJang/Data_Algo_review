e94ed1ca1283ea4a83f3f3f21b25bc1fedd82b0e
Jang <hansung.j1106@gmail.com>
2024-08-30 19:12:06 +0900

    update - 2798.cpp (새로운 풀이 & 제출답안)

--- BOJ_step_sol/step12_brute_force/2798.cpp
+++ BOJ_step_sol/step12_brute_force/2798.cpp
@@ -1,6 +1,7 @@
 /*
-1. idea.
 
+# 1-trial: 
+1. idea.
 
 배열 만들기 
 DP 문제 
@@ -21,23 +22,78 @@ col: n-1_C_2 = (n-1) * (n-2) / 2
 
 3. 자료구조 
 
+# 2-trial; 
+1. 아이디어: 조합수 만큼 3중 for-loop 
+
+- n_C_3; 전체 가능한 카드의 조합 / 횟수 
+- 3중 for-loop으로 1번째(0 ~ n-2), 2번째 (i+1 ~ n-1), 3번째(j+1 ~ n)
+// 각 loop의 구조는 안의 인덱스는 무조건 앞의 인덱스보다 커야함. 
+- sort 함수를 통해서 찾은값, 내림차순으로 정렬 
+- for loop 돌면서 Max값 확인 
+
+2. 시간복잡도: 
+- TC(N^3)
+카드의 수에 의해서 TC 결정 (MAX: 100)
+100_C_3 = (98 * 99 * 100) / (2 * 3) = 161,700  << 1억 
+>> 3중 Loop 사용가능
+
+3. 자료구조 
+- vector: 
+- 3중 for-loop 
 */
 
 #include <iostream>
 #include <vector>
+#include <algorithm> // sort 
+#define card_fac 6 // card factorial value 
+
 
 using namespace std; 
 
+int SIZE(int size)
+{
+    int result = 1;
+    for(int i = (size -2); i <= size; i++){result *= i;}
+    result /= card_fac;  
+    return result; 
+}
+
 int main()
 {
-
-    vector<vector<int>> card; 
-    int win = 0; 
+    int size; 
+    vector<int> card; 
+    vector<int> sum_list; 
+    int max = 0; 
     int N, M; // N: card 갯수, M: target값 
 
     cin >> N >> M; 
-    
+    size = SIZE(N); 
 
+    for(int i = 0; i < N; i++)
+    {
+        int elem; 
+        cin >> elem; 
+        card.push_back(elem); 
+    }
+
+    sort(card.begin(), card.end()); 
+
+    for(int i = 0; i < N -2; i++)
+    {
+        for(int j = i+1; j < N-1; j++)
+        {
+            for(int k = j+1; k < N; k++)
+            {
+                int elem = card[i] + card[j] + card[k]; 
+                if(elem > M){break;} 
+                sum_list.push_back(elem); 
+            }
+        }
+    }
+
+    sort(sum_list.begin(), sum_list.end()); 
+
+    cout << sum_list.back(); 
 
     return 0; 
 }
